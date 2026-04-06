5a3b52fe018b5316be28722006fffc2799561b43
Goose <lnpscoop@gmail.com>
2026-04-07 04:51:26 +0500

    Произведён рефакторинг. Подключена база данных

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,19 +2,21 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.Validation.UserValidator;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.User.UserStorage;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
+@Validated
 public class UserController {
     private final UserStorage userStorage;
     private final UserService userService;
@@ -26,17 +28,42 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> addUser(@Validated @RequestBody User user) {
+    public ResponseEntity<User> addUser(@RequestBody User user) {
         log.info("Создание пользователя: {}", user.getLogin());
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        UserValidator validator = new UserValidator();
+        validator.validateUser(user);
+
         User savedUser = userStorage.addUser(user);
         log.info("Пользователь '{}' успешно создан, ID: {}", user.getLogin(), savedUser.getUserId());
-        return ResponseEntity.status(201).body(savedUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        log.info("Обновление пользователя с ID: {}", user.getId());
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        UserValidator validator = new UserValidator();
+        validator.validateUser(user);
+
+        User updatedUser = userStorage.updateUser(user);
+        log.info("Пользователь с ID {} успешно обновлён", user.getId());
+        return ResponseEntity.ok(updatedUser);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable int id, @Validated @RequestBody User user) {
+    public ResponseEntity<User> updateUserById(@PathVariable int id, @Validated @RequestBody User user) {
         log.info("Обновление пользователя с ID: {}", id);
-        user.setUserId(id);
+        user.setId(id);
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
         User updatedUser = userStorage.updateUser(user);
         log.info("Пользователь с ID {} успешно обновлён", id);
         return ResponseEntity.ok(updatedUser);
@@ -51,15 +78,15 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<User>> getUserById(@Validated @PathVariable int id) {
-        log.info("Получение пользователя по ID");
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        log.info("Получение пользователя по ID: {}", id);
         User user = userStorage.getUserById(id);
         log.debug("Пользователь с ID {} найден!", id);
-        return ResponseEntity.ok(Optional.ofNullable(user));
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Optional<User>> deleteUser(@Validated @PathVariable int id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
         log.info("Удаление пользователя с ID {}", id);
         userStorage.deleteUser(id);
         log.info("Пользователь с ID {} успешно удалён", id);
@@ -67,7 +94,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public ResponseEntity<Void> addFriend(@PathVariable int id, @PathVariable Integer friendId) {
+    public ResponseEntity<Void> addFriend(@PathVariable int id, @PathVariable int friendId) {
         log.info("Добавление друга: пользователь с id {} добавляет пользователя с id {}", id, friendId);
         userService.addFriend(id, friendId);
         return ResponseEntity.ok().build();
